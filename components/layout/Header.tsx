6408fcf93899e270c49704fce877cf0a9869b58e
Rodney Pattison <editorial@globaltravelreport.com>
2025-05-10 15:43:20 +0700

    Remove duplicate Categories link from header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -86,13 +86,6 @@ export function Header() {
               Home
             </Link>
 
-            <Link
-              href="/categories"
-              className={`text-[#C9A14A] hover:text-white transition-colors ${isActive('/categories') ? 'font-bold underline' : ''}`}
-            >
-              Categories
-            </Link>
-
             {/* Country Dropdown */}
             <div className="relative">
               <button
@@ -270,14 +263,6 @@ export function Header() {
                 onClick={() => setIsMenuOpen(false)}
               >
                 Home
-              </Link>
-
-              <Link
-                href="/categories"
-                className={`text-[#C9A14A] hover:text-white transition-colors ${isActive('/categories') ? 'font-bold underline' : ''}`}
-                onClick={() => setIsMenuOpen(false)}
-              >
-                Categories
               </Link>
 
               <div className="space-y-2">
