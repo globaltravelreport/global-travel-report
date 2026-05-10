a8db1ed28743c1e9a35b7ebd17956b034790c7ee
InterlineAsia <admin@interlineasia.com>
2026-05-11 09:08:23 +1000

    Remove stale duplicate source files

--- src/utils/bundle-optimization.ts
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-
-export function dynamicImportWithLoading(importFn: () => Promise<any>, loading?: () => React.ReactElement) {
-  return dynamic(importFn, { ssr: false, loading });
-}
-// Add more helpers as needed for chunk naming, modularized imports, etc.
